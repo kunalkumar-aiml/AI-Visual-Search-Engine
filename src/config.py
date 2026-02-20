87573c7cb3f048e541b2fca9e1ccf4862e30c8b2
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-02-20 11:25:01 +0530

    Add project configuration file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# Project Configuration File
+
+PROJECT_NAME = "AI Visual Search Engine"
+
+IMAGE_SIZE = (96, 96)
+BATCH_SIZE = 32
+EPOCHS = 5
+NUM_CLASSES = 10
+
+MODEL_SAVE_PATH = "models/classifier.h5"
